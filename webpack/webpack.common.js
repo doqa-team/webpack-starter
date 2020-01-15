242fe4cd0e59c5b646533e87f0ee74580712e3d6
Pavel Zinovev <zombiqwerty@yandex.ru>
2020-01-15 11:21:33 +0300

    Added html-loader, updated @ittinc/stylelint

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -67,6 +67,12 @@ module.exports = {
           },
         },
       },
+      {
+        test: /\.(html)$/,
+        use: {
+          loader: 'html-loader',
+        },
+      },
     ],
   },
 };
